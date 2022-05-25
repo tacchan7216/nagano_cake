b90d4b1365297d7b39919d382d91a3e728d690f8
tacchan7216 <104713690+tacchan7216@users.noreply.github.com>
2022-05-25 01:40:11 +0000

    [Update]item-view-index&show

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -5,13 +5,16 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.save
-    redirect_to admin_item_path(@item.id)
+    if @item.save
+      redirect_to admin_items_path
+    else
+      render :new
+    end
   end
 
   def index
     @item = Item.new
-    @items = Item.all
+    @items = Item.page(params[:page])
   end
 
   def show
